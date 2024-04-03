ff793ae7d6ce3d885f020a1f7174672a747e9b5c
Oleg <yakushinoa@gmail.com>
2024-04-03 18:05:50 +0200

    Initial Commit

---
+++ Striker/StrikerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrikerApp.swift
+//  Striker
+//
+//  Created by Oleg Yakushin on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StrikerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
